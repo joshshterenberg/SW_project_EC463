f9b308019e3e03606f0598ca51d3824cba3a0aba
joshshterenberg <joshuashterenberg@gmail.com>
2022-09-15 21:55:56 -0400

    login page comms update

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      account: {
+        accountName: props.accountName
+      }
+    }
+  }
+  handleChange(event) {
+    var account = this.state.account;
+    account.accountName = event.target.value;
+    this.setState({account: account });
+  }
+  handleClick() {
+    console.log(this.state.account);
+  }
   render() {
     return (
       <>
@@ -13,6 +29,8 @@ export default class Login extends Component {
           <label>User's Twitter Handle</label>
           <input
             type="text"
+            value={this.state.account.accountName}
+            onChange={this.handleChange.bind(this)}
             className="form-control"
             placeholder="Enter Twitter handle (ex. @john_doe)"
           />
@@ -20,7 +38,7 @@ export default class Login extends Component {
 
 	<Link to={'/single'}>
         <div className="d-grid">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary" onClick={this.handleClick.bind(this)}>
             Analyze
           </button>
         </div>
